9d4c1f170165a4a12f4eeffcdd51e54177b68468
developer-bagic-02 <ketanexam92@gmail.com>
2024-06-27 18:43:50 +0530

    Update WebDemoApplication.java

--- src/main/java/com/web/WebDemoApplication.java
+++ src/main/java/com/web/WebDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("kkkkkkkkkkkkkkk");
 		SpringApplication.run(WebDemoApplication.class, args);
 	}
 
