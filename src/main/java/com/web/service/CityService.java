fe2927242e5d8e5d38244b76c2bc6598aedadbf4
developer-bagic-02 <ketanexam92@gmail.com>
2024-06-28 03:45:37 -0700

    added ghansoli

--- src/main/java/com/web/service/CityService.java
+++ src/main/java/com/web/service/CityService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class CityService {
 
 	public String getCities() {
-		return "Mumbai,Vashi,Thane,Panvel,Ghansoli,Vikroli,kandivali,Rabale,vashi,Airolimgt,Sanpada,pune,Nerul,Belapur,thane,pune";
+		return "Mumbai,Vashi,Thane,Panvel,Ghansoli,Vikroli,kandivali,Rabale,vashi,Airolimgt,Sanpada,pune,Nerul,Belapur,thane,pune,ghansoli";
 	}
 	
 	
